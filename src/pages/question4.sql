9ea1afa8ac13902e004fb8a6b19d400962fe0868
Jim Powell <76704500+PowellJim@users.noreply.github.com>
2021-07-30 14:49:19 -0700

    Added SQL question

---
+++ src/pages/question4.sql
@@ -0,0 +1,17 @@
+-- Setup
+
+-- Assume DB = postgresql
+
+-- Given the table:
+
+CREATE TABLE something
+(
+    id uuid NOT NULL,
+    name  CHARACTER VARYING(255) NOT NULL,
+    email CHARACTER VARYING(255) NOT NULL,
+    phone CHARACTER VARYING(255),
+    birthday DATE,
+    age INTEGER
+);
+
+-- create a trigger/function which will calculate the age given the birthdate
